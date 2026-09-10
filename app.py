fc3b92b88ee20ed3df6d2c8f21c9bfadaeee713d
23a31a05f5 <kalyanilakshmipolisetty05@gmail.com>
2026-09-10 16:37:57 +0530

    day8

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,session
 from db import get_db_connection
 app = Flask(__name__)
 
@@ -14,18 +14,21 @@ def recharge():
     if request.method == "POST":
 
         mobile = request.form["mobile"]
+        operator=request.form["operator"]
         amount = request.form["amount"]
+        
         connection=get_db_connection()
         cursor=connection.cursor()
-        sql="""INSERT INTO recharge(mobile,amount)
-        values(%s,%s)"""
+        sql="""INSERT INTO recharges(mobile,operator,amount)
+        values(%s,%s,%s)"""
         values=(mobile,
+                operator,
                 amount)
         cursor.execute(sql,values)
         connection.commit()
         cursor.close()
         connection.close()
-        print("Recharge history updated")
+        return render_template("successrecharge.html")
 
 
     return render_template("recharge.html")
@@ -76,6 +79,28 @@ def register():
         connection.close()
         return render_template("successregister.html")
     return render_template("register.html")
+@app.route("/login",methods=["GET","POST"])
+def login():
+    if request.method=="POST":
+        email=request.form["email"]
+        password=request.form["password"]
+        db=get_db_connection()
+        cursor=db.cursor()
+        sql="""select id,name from users 
+        where email=%s and password=%s"""
+        values=(email,password)
+        cursor.execute(sql,values)
+        user=cursor.fetchone()
+        cursor.close()
+        db.close()
+        if user:
+            session["user_id"]=user[0]
+            session["user_name"]=user[1]
+            return render_template("dashboard.html")
+        else:
+            return "Invalid email or password"
+    return render_template("login.html")
+
 @app.route("/history")
 def history():
     return render_template("history.html")
@@ -85,5 +110,5 @@ def history():
 def about():
     return render_template("about.html")
 
-
+app.secret_key = "akRiva_secret_key_2026"
 app.run(debug=True)
